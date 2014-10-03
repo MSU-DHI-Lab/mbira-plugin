5f3c069d7953d4c5c9171aad1c86aca5595c938a
austin.truchan <austin.truchan@01706023-bb97-4ce8-ab48-9f15e5b5d7ad>
2014-10-03 19:43:15 +0000

    NewProject Sytling
    git-svn-id: http://svn.matrix.msu.edu/svn/mbira_plugin@13 01706023-bb97-4ce8-ab48-9f15e5b5d7ad

--- newproject.php
+++ newproject.php
@@ -2,26 +2,32 @@
 <html>
 <head>
 	<script src="js/script.js"></script> 
-	
+	<link rel="stylesheet" type="text/css" href="css/newproject.css"/>
 </head>
 <body>
+    
+     <!--HEADER-->
+    <div class="header">
+        <div class="back"><a href="project_all.html"><img src="img/back.png"/><p>ALL PROJECTS</p></a></div>
+        <div class="title"><h1>NEW PROJECT</h1></div></div>
+    
 	<div class="main" ng-controller="newProjectCtrl">
-		<h1>New Project</h1>
-		<form id="newprojectform" name="newprojectform" novalidate ng-submit="submit()" role="form">
-			<div class="form-group">				        
-					<input type="text" required class="form-control npInput" id="name" name="name" ng-model="newProject.name" placeholder="Project Name">
-			</div>
+        
+        <div class="thumbnail"> <a href="thumbnail.html"> <img src="img/thumbnail_empty.png" height="340" width="340">
+            <div class="thumbnail_title"><h3>ADD THUMBNAIL</h3></div></a></div>
+        
+    <div class="project_information">    
+        <form id="newprojectform" name="newprojectform" novalidate ng-submit="submit()" role="form">
+          
+            <div class="form-group">				        
+				<input type="text" required class="form-control npInput" id="name" name="name" ng-model="newProject.name" placeholder="ENTER PROJECT TITLE HERE"></div>
 			
-			<div class="form-group">				        
-					<input type="text" required class="form-control npInput" id="description" name="description" ng-model="newProject.description" placeholder="Project Description">
-			</div>
+            <div class="form-group">				        
+					<input type="text" required class="form-control npInput" id="description" name="description" ng-model="newProject.description" placeholder="ENTER PROJECT DESCRIPTION HERE"></div>
 			
-			<div class="form-group">				        
-					<input type="file" id="fileSelect" name="file" ng-file-select="onFileSelect($files)"/>
-			</div>
-			
-			<button type="submit" id="submit" class="btn btn-default" ng-disabled="newprojectform.$invalid" ng-class="{'btn-disabled': newprojectform.$invalid}">Create Project</button>
-		</form
-	</div>
+            <button type="submit" id="submit" class="btn btn-default" ng-disabled="newprojectform.$invalid" ng-class="{'btn-disabled': newprojectform.$invalid}">CREATE NEW PROJECT</button>
+		</form></div></div>
 </body>
 </html>
+    
+ 
