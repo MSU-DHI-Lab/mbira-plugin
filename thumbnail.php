dd4a460556c8696bafd9f0228c62d6cb747f1b85
austin.truchan <austin.truchan@01706023-bb97-4ce8-ab48-9f15e5b5d7ad>
2014-10-03 21:17:17 +0000

    ADD THUMBNAIL PAGE
    git-svn-id: http://svn.matrix.msu.edu/svn/mbira_plugin@16 01706023-bb97-4ce8-ab48-9f15e5b5d7ad

---
+++ thumbnail.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/thumbnail.css"/>
+</head>
+<body>
+    
+     <!--HEADER-->
+    <div class="header">
+        <div class="back"><a href="project_all.html"><img src="img/back.png"/><p>*PREVIOUS PAGE TITLE*</p></a></div>
+        <div class="title"><h1>ADD THUMBNAIL</h1></div></div>
+    
+    
+    <!--DROPZONE-->
+	<div class="dropzone">
+    <div class="dropzone_content">
+        <h2>DROP IMAGE HERE <br><br> OR</h2>
+        <form>
+            <div class="form-group">				        
+					<input type="file" id="fileSelect" name="file" ng-file-select="onFileSelect($files)"/>
+			</div>
+            <div class="form-group2">				        
+					<input type="file" id="fileSelect" name="file" ng-file-select="onFileSelect($files)"/>
+			</div></form></div>
+   </div>
+</body>
+</html>
+    
+ 
