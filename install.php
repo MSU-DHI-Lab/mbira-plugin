acc192f024034b1706cd25762317640759f9bcb2
jacob.buckley <jacob.buckley@01706023-bb97-4ce8-ab48-9f15e5b5d7ad>
2014-12-12 19:58:17 +0000

    Added locations to database install
    git-svn-id: http://svn.matrix.msu.edu/svn/mbira_plugin@34 01706023-bb97-4ce8-ab48-9f15e5b5d7ad

--- install.php
+++ install.php
@@ -10,14 +10,33 @@
 	// sql to create table
 	$sql = "CREATE TABLE `$dbname`.`mbira_projects`(
 	`id` INTEGER (10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(5000) NOT NULL,
-	`description` VARCHAR(5000) NOT NULL,
-	`image_path` VARCHAR(300) NOT NULL,
+	`name` VARCHAR(5000),
+	`description` VARCHAR(5000),
+	`image_path` VARCHAR(300),
 	PRIMARY KEY (`id`)
 	)";
 	
 	if (mysqli_query($con, $sql)) {
 		echo "Table mbira_projects created successfully";
+	} else {
+		echo "Error creating table: " . mysqli_error($con);
+	}
+	
+	// sql to create table
+	$sql = "CREATE TABLE `$dbname`.`mbira_locations`(
+	`id` INTEGER (11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`project_id` INTEGER(11),
+	`name` VARCHAR(200) NOT NULL,
+	`description` VARCHAR(500),
+	`exhibit_id` INTEGER(11),
+	`latitude` VARCHAR(100),
+	`longitude` VARCHAR(100),
+	`file_path` VARCHAR(500),
+	PRIMARY KEY (`id`)
+	)";
+	
+	if (mysqli_query($con, $sql)) {
+		echo "Table mbira_locations created successfully";
 	} else {
 		echo "Error creating table: " . mysqli_error($con);
 	}
