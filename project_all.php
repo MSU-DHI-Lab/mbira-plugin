1cde81295f04c02efdc757cdc12878b0b72bed98
austin.truchan <austin.truchan@01706023-bb97-4ce8-ab48-9f15e5b5d7ad>
2015-02-03 21:58:28 +0000

    Styling to All Project Menu Page
    git-svn-id: http://svn.matrix.msu.edu/svn/mbira_plugin@46 01706023-bb97-4ce8-ab48-9f15e5b5d7ad

--- project_all.php
+++ project_all.php
@@ -12,7 +12,7 @@
     <div class="projects" ng-controller='viewProjectsCtrl'>
 		<div class="project" ng-repeat='project in projects'> 
 			<a ui-sref="viewProject"> 
-				<img ng-src="images/{{project.image_path}}" height="340" width="340">
+				<img ng-src="images/{{project.image_path}}" height="225" width="225">
 				<div class="project_title"><h3>{{project.NAME}}</h3></div>
 			</a>
 		</div>
